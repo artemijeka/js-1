d69f3b4e3282c7b5d2a794db56e372a29f6691ab
artemijeka <artem.kuznecov.samara@gmail.com>
2019-09-22 10:20:35 +0400

    5-v.3

---
+++ 5/v.1/logic.js
@@ -0,0 +1,16 @@
+  function createChessBoard() {
+
+    // создаем таблицу
+    var table = document.createElement("table");
+    // задаем ей класс для стилизации
+    table.className = "table-theme";
+    // помещаем внутрь строки и столбцы
+    table.innerHTML = "<tr><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td></tr><tr><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td></tr><tr><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td></tr><tr><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td></tr><tr><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td></tr><tr><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td></tr><tr><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td></tr><tr><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td><td><span></span></td></tr>";
+    // помещаем таблицу в конец body
+    document.body.appendChild(table);
+    
+  }
+  // при загрузке страницы разрешить выполнение createChessBoard
+  Window.onload = createChessBoard;
+  // запустить выполнение функции
+  createChessBoard();
